2dda293ba526d44fe12990d807122560fd7c2ebf
hoichong <hoichong@w3ocean.com>
2023-07-13 22:46:05 +0800

    fix pub mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,17 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-mod fib_calcs;
+pub mod fib_calcs;
 
 use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
 
 use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
 
-pub mod fib_numbers;
+//pub mod fib_numbers;
 
 #[pyfunction]
 fn say_hello() {
     println!("saying hello from Rust!");
-}
-
-#[pymodule]
-fn hc_fib_rust_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(say_hello));
-    Ok(())
 }
 
 #[pymodule]
