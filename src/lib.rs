8221c174c85d3d2effdc5a073996526ceb775413
hoichong <hoichong@w3ocean.com>
2023-07-27 01:15:49 +0800

    mock add vectors

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-pub mod interface;
 pub mod fib_calcs;
+pub mod interface;
 
 use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
 
@@ -16,8 +16,25 @@ pub mod class_module;
 
 use class_module::fib_processor::FibProcessor;
 
-
 //pub mod fib_numbers;
+
+#[pyfunction]
+
+fn time_add_vectors(total_vector_size: i32) -> Vec<i32> {
+    let mut buffer: Vec<i32> = Vec::new();
+
+    let first_vector: Vec<i32> = (0..total_vector_size.clone()).map(|x| x).collect();
+
+    let second_vector: Vec<i32> = (0..total_vector_size).map(|x| x).collect();
+
+    for i in &first_vector {
+        buffer.push(first_vector[**&i as usize] + second_vector[*i as usize]);
+    }
+
+    return buffer;
+}
+
+
 
 #[pyfunction]
 fn say_hello() {
@@ -37,6 +54,8 @@ fn hc_fib_rust_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(object_interface));
 
     m.add_class::<FibProcessor>()?;
+
+    m.add_wrapped(wrap_pyfunction!(time_add_vectors));
 
     Ok(())
 }
